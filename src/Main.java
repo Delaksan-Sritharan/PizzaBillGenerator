8bffa031a39a56a0b311e8504987d0a3d4a55cb9
Delaksan-Sritharan <delaksan@gmail.com>
2025-09-09 11:25:19 +0530

    Enhance Pizza class with extra toppings, cheese, and take away options

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Pizza basePizza = new Pizza(false);
-        basePizza.getPizzaPrice();
+        basePizza.addExtraToppings();
+        basePizza.addExtraCheese();
+        basePizza.takeAway();
+        basePizza.getBill();
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -2,6 +2,11 @@ public class Pizza {
     private int price;
     private Boolean veg;
 
+    private int extraCheesePrice = 100;
+    private int extraToppingsPrice = 150;
+    private int backPackPrice = 20;
+    private int basePizzaPrice;
+
     public Pizza(Boolean veg){
         this.veg = veg;
         if(this.veg){
@@ -9,10 +14,30 @@ public class Pizza {
         }else {
             this.price = 400;
         }
+        basePizzaPrice = this.price;
     }
 
-    public void getPizzaPrice(){
-        System.out.println(this.price);
+    public void addExtraCheese(){
+        System.out.println("Extra Cheese Added");
+        this.price += extraCheesePrice;
+    }
+
+    public void addExtraToppings(){
+        System.out.println("Extra Toppings Added");
+        this.price += extraToppingsPrice;
+
+    }
+
+    public void takeAway(){
+        System.out.println("Take Away Opted");
+        this.price += backPackPrice;
+
+    }
+
+    public void getBill(){
+        System.out.println("Pizza: "+basePizzaPrice);
+
+
     }
 
 }
